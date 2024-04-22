3f28805dcd1e640757aedcb006bd8126677ef363
Willian Almeida <willianac01@gmail.com>
2024-04-22 10:00:05 -0300

    refactor: add NODE_ENV prop to handle values of both dev and prod environments

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
+import "dotenv/config";
 import Fastify from "fastify";
 import xmlParser from "fastify-xml-body-parser";
 import formParser from "@fastify/formbody";
@@ -10,7 +11,10 @@ fastify.register(xmlParser);
 fastify.register(formParser);
 fastify.register(routes);
 
-fastify.listen({port: 3000}, function(err) {
+const PORT = process.env.PORT || 3000;
+const HOST = process.env.NODE_ENV === "prod" ? "0.0.0.0" : "127.0.0.1";
+
+fastify.listen({port: Number(PORT), host: HOST}, function(err) {
 	if(err) {
 		console.log(err);
 		process.exit(1);
