83ee491d1c8654d532752ea1c098b43080f5731c
Willian Almeida <willianac01@gmail.com>
2024-04-25 21:07:28 -0300

    refactor: create type for Xpresso Payload

---
+++ src/types/XpressoPayload.ts
@@ -0,0 +1,24 @@
+export type XpressoPayload = {
+	Sender_firstName: string
+	Sender_lastName: string
+	Sender_gender: string
+	Sender_dateOfBirth: string
+	Sender_countryOfBirth: string
+	Sender_idNumber: string
+	Sender_idType: string
+	Sender_nationality: string
+	Sender_occupation: string
+	Sender_fiId: string
+	Sender_countryCode: string
+	Sender_disableScreening: string
+	Receiver_firstName: string
+	Receiver_lastName: string
+	Receiver_accountNumber: string
+	Receiver_fiId: string
+	Receiver_countryCode: string
+	receiveAmt: string
+	receiveAmtCcy: string
+	purpose: string
+	relationship: string
+	sourceOfFunds: string
+}
