007bff78c7d2c64259d1182db0799c1f73b3a95f
Willian Almeida <willianac01@gmail.com>
2024-04-30 14:50:11 -0300

    feat: 'xp/9714' endpoint now returns rate and status in xml format

--- src/api/controllers/xpresso/xpresso.ts
+++ src/api/controllers/xpresso/xpresso.ts
@@ -1,4 +1,6 @@
 import { FastifyInstance } from "fastify";
+import { handleTransaction } from "../../handlers/handleTransaction.js";
+import { XpressoPayload } from "../../../types/XpressoPayload.js";
 import { js2xml } from "xml-js";
 
 const routes = async (server: FastifyInstance) => {
@@ -8,11 +10,11 @@ const routes = async (server: FastifyInstance) => {
 	
 	server.post("/xpresso/xp/9714", async function(req, rep) {
 		const body = req.body as object;
-		const jsObject = {...body};
-		const xmlData = js2xml(jsObject, {compact: true});
-
+		const transactionObj = {...body as XpressoPayload};
+		const result = await handleTransaction(transactionObj);
+		const xml = js2xml(result, {compact: true});
 		rep.type("text/xml");
-		rep.send(xmlData);
+		rep.send(xml);
 	});
 };
 
