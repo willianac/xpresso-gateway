9d5ea06e310db24a31b27791b0608ffd4c93099a
Willian Almeida <willianac01@gmail.com>
2024-05-01 15:37:02 -0300

    feat: create function to check transaction status

---
+++ src/api/controllers/almond/checkTransactionStatus.ts
@@ -0,0 +1,21 @@
+import fetch from "node-fetch";
+
+type Status = "RJCT" | "TSFS" | "TSFR" | "PNDG" | "PRCD" | "RVSD" | "ACCP"
+
+type CheckTransactionStatusResponse = {
+  transactionStatus: Status
+  transactionCancellationStatus: string | null,
+  reversal: string | null,
+  cashoutStatus: string | null
+}
+
+export async function checkTransactionStatus(id: string, accessToken: string) {
+	const res = await fetch(process.env.ALMOND_SANDBOX_URL + `/v1/transactions/${id}/status`, {
+		method: "GET",
+		headers: {
+			"Authorization": "Bearer " + accessToken,
+		},
+	});
+	const data = await res.json() as CheckTransactionStatusResponse;
+	return data;
+}
