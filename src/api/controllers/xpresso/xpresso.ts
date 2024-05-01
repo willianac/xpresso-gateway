9fd90fa83e807c18c64b9fc28cc9204afaabe6f6
Willian Almeida <willianac01@gmail.com>
2024-05-01 15:32:23 -0300

    refactor: 'xp/9714' endpoint will imediately respond with 200

--- src/api/controllers/xpresso/xpresso.ts
+++ src/api/controllers/xpresso/xpresso.ts
@@ -1,7 +1,6 @@
 import { FastifyInstance } from "fastify";
 import { handleTransaction } from "../../handlers/handleTransaction.js";
 import { XpressoPayload } from "../../../types/XpressoPayload.js";
-import { js2xml } from "xml-js";
 
 const routes = async (server: FastifyInstance) => {
 	server.get("/", async function(req, rep) {
@@ -11,10 +10,8 @@ const routes = async (server: FastifyInstance) => {
 	server.post("/xpresso/xp/9714", async function(req, rep) {
 		const body = req.body as object;
 		const transactionObj = {...body as XpressoPayload};
-		const result = await handleTransaction(transactionObj);
-		const xml = js2xml(result, {compact: true});
-		rep.type("text/xml");
-		rep.send(xml);
+		handleTransaction(transactionObj);
+		rep.status(200).send();
 	});
 };
 
