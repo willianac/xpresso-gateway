ef9f0a8c9435182a7306262549818d6ad0ecc9c0
Willian Almeida <willianac01@gmail.com>
2024-05-01 15:30:57 -0300

    refactor: remove error catching from controllers

--- src/api/controllers/almond/processTransaction.ts
+++ src/api/controllers/almond/processTransaction.ts
@@ -6,16 +6,12 @@ type ProcessTransactionResponse = {
 }
 
 export async function processTransaction(id: string, accessToken: string) {
-	try {
-		const res = await fetch(process.env.ALMOND_SANDBOX_URL + `/v1/transactions/${id}/process`, {
-			method: "POST",
-			headers: {
-				"Authorization": "Bearer " + accessToken
-			}
-		});
-		const data = await res.json() as ProcessTransactionResponse;
-		return data;
-	} catch (error) {
-		console.log(error);
-	}
+	const res = await fetch(process.env.ALMOND_SANDBOX_URL + `/v1/transactions/${id}/process`, {
+		method: "POST",
+		headers: {
+			"Authorization": "Bearer " + accessToken
+		}
+	});
+	const data = await res.json() as ProcessTransactionResponse;
+	return data;
 }
