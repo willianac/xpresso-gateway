39139f83b2e35a4312738035738369452c189bea
Willian Almeida <willianac01@gmail.com>
2024-09-26 10:06:46 -0300

    feat: remove BRL from rates routine

--- src/routines/updateRates.ts
+++ src/routines/updateRates.ts
@@ -8,7 +8,7 @@ import { generateFTPFile } from "../utils/generateFTPFile.js";
 export async function writeRatesToFTP() {
 	const token = (await getAcessToken(true)).access_token;
 	const sourceCurrency = "USD";
-	const targetCurrencies = ["PHP", "BRL"];
+	const targetCurrencies = ["PHP"];
 
 	const rateList: string[][] = [];
 
